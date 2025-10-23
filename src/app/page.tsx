b0bdf21a324912ccf8c039023f3f29cdc81a0e31
mehdi <mehdihassan78900@gmail.com>
2025-10-23 22:47:32 +0500

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Aboutpage from "./about/page";
-import Navbar from "@/components/home/Navbar";
+
 import Hero from "@/components/home/Hero";
-import Footer from "@/components/home/Footer";
 
 const Page = () => {
   return (
